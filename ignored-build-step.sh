91c9be6e282cc9e4fb9ad88869dd1c03fd854293
Logan Call <mail@logancall.com>
2021-04-12 16:10:28 -0700

    add ignore build step script

---
+++ ignored-build-step.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+# "staging" and "prod" are actual git branches, so if you need a different branch to build on vercel you can add another or statement here with the new branch name
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "prod" ]] ; then
+  # Proceed with the build
+    echo "✅ - Build can proceed"
+  exit 1;
+
+else
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
