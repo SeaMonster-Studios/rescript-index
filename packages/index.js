1b72efa13952e031aceb167810e45f8e4b23b4d6
Logan Call <mail@logancall.com>
2021-06-25 15:54:00 -0700

    replace snowpack with webpack from cra

--- packages/index.js
+++ packages/index.js
@@ -2,12 +2,8 @@ import "./index.css"
 import React from "react"
 import * as ReactDOM from "react-dom"
 
-window.location = "http://localhost:5000"
-
 const App = () => {
-  return <p>
-    This is not the page you're looking for. Make sure that you're running both snowpack (which shows this root) as well as cosmos which both proxies this root and provides the URL you should be viewing. If you ran the `yarn docs` script and landed here, try <a href="http://localhost:5000">http://localhost:5000</a> instead.
-      </p>
+  return <p>This is not the page you're looking for.</p>
 }
 
 ReactDOM.render(<App />, document.getElementById("root"))
